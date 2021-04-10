b5c1f36989a6edb3c83f23f4b10e8e527a97a7bd
Lucas <lucasvalenteds@gmail.com>
2021-04-10 00:22:42 -0300

    Implementing Command Line Interface (CLI)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
 mod pattern;
 
-fn main() {}
+use pattern::*;
+use std::env;
+use std::process::exit;
+
+fn print_help(filename: String) {
+    println!("Usage: {} <delimiter> <mask> <raw_code>", filename);
+    println!("Example: {} - xx-xx-xxxx 12052005", filename);
+}
+
+fn print_error() {
+    println!("One or more arguments missing!");
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let (raw_code, pattern) = match args.len() {
+        1 => {
+            print_help(args[0].to_string());
+            exit(0);
+        }
+        2 | 3 => {
+            print_error();
+            exit(1);
+        }
+        _ => {
+            let pattern = Pattern {
+                delimiter: String::from(&args[1]),
+                mask: String::from(&args[2]),
+            };
+
+            let raw_code = String::from(&args[3]);
+
+            (raw_code, pattern)
+        }
+    };
+
+    match pretty_print(raw_code.to_owned(), pattern) {
+        Some(code) => {
+            println!("{}", code);
+            exit(0);
+        }
+        None => {
+            println!("Could not pretty print the code: {}", raw_code);
+            exit(2);
+        }
+    }
+}
---
+++ tests/cli.rs
@@ -0,0 +1,53 @@
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+use rstest::*;
+use std::error::Error;
+use std::process::Command;
+
+#[test]
+fn showing_help() -> Result<(), Box<dyn Error>> {
+    let mut cmd = Command::cargo_bin("code-pattern")?;
+
+    cmd.assert()
+        .success()
+        .code(0)
+        .stdout(predicates::str::is_empty().not());
+
+    Ok(())
+}
+
+#[rstest]
+#[case("")]
+#[case("-")]
+#[case("- x-x-x")]
+fn missing_cli_arguments(#[case] args: &str) -> Result<(), Box<dyn Error>> {
+    let mut cmd = Command::cargo_bin("code-pattern")?;
+
+    cmd.args(args.split(" ").into_iter());
+    cmd.assert()
+        .failure()
+        .code(1)
+        .stdout(predicates::str::contains("One or more arguments missing!"));
+
+    Ok(())
+}
+
+#[rstest]
+#[case("- x-x-x 123", "1-2-3")]
+#[case("- x-xx-xxx 123456", "1-23-456")]
+#[case("; x;xx;xxx 123456", "1;23;456")]
+#[case("; x;xx;xxx;xx;x abcdefghi", "a;bc;def;gh;i")]
+fn pretty_printing_valid_code(
+    #[case] args: &str,
+    #[case] code: &str,
+) -> Result<(), Box<dyn Error>> {
+    let mut cmd = Command::cargo_bin("code-pattern")?;
+
+    cmd.args(args.split(" ").into_iter());
+    cmd.assert()
+        .success()
+        .code(0)
+        .stdout(predicate::str::contains(code));
+
+    Ok(())
+}
